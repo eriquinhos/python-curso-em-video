5dd1c145ff210a4867db2b5031bf2f8743c2b139
Eric Victor <ericvictorsilva.es@gmail.com>
2023-11-16 03:46:06 -0300

    update ex. 11

--- mundo_1/Exercício 11.py
+++ mundo_1/Exercício 11.py
@@ -1,8 +1,8 @@
 # Make a program to calculate the area of a wall and how much paint you will need to paint it
 # 1L of paint = 2m²
 
-try:  # In this part, the user should type the values of height and length of the single wall it wants to paint
-    height = float(input('Type the height of your wall: '))
+try:  # In this part, the user should type the values of HEIGHT and length of the single wall it wants to paint
+    height = float(input('Type the HEIGHT of your wall: '))
     length = float(input('Type the length of your wall: '))
 except ValueError:  # If one of the values are not valid, the message below must appear on the screen
     print('Invalid Value. Please, try again.')
